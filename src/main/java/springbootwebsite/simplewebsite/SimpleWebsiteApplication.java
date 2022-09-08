3f4eb79bb5eaced15e534b5c6408f6f628910ab4
bahamin <bahamin61@yahoo.com>
2022-09-07 22:30:50 -0700

    kafka changes

--- src/main/java/springbootwebsite/simplewebsite/SimpleWebsiteApplication.java
+++ src/main/java/springbootwebsite/simplewebsite/SimpleWebsiteApplication.java
@@ -2,10 +2,15 @@ package springbootwebsite.simplewebsite;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
 
 @SpringBootApplication
 public class SimpleWebsiteApplication extends SpringBootServletInitializer {
@@ -22,4 +27,8 @@ public class SimpleWebsiteApplication extends SpringBootServletInitializer {
         logger.info("Application has been started successfully");
     }
 
+    @KafkaListener(topics = "KafkaMessageLogger", groupId = "group-id")
+    public void listen(String message) {
+        logger.info ("Received Messasge in group - group-id: " + message);
+    }
 }
--- src/main/java/springbootwebsite/simplewebsite/controller/InfoController.java
+++ src/main/java/springbootwebsite/simplewebsite/controller/InfoController.java
@@ -1,6 +1,9 @@
 package springbootwebsite.simplewebsite.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,21 +16,28 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("info")
 public class InfoController {
+    private static final Logger logger = LogManager.getLogger(InfoController.class);
 
     @Autowired
     private InfoServiceUsingJpa service;
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @RequestMapping(path = "/getDevelopertName", method = RequestMethod.GET)
-    public String getDeveloperName(){
+    public String getDeveloperName() {
         return "Bahamin Shahsavand";
     }
 
     @RequestMapping(path = "/getContent", method = RequestMethod.POST)
-    public String getContent(HttpServletRequest request, @RequestBody ContentRequestDTO contentRequest){
+    public String getContent(HttpServletRequest request, @RequestBody ContentRequestDTO contentRequest) {
         String content = "";
 
-       // content = service.getContent(contentRequest);
+        // content = service.getContent(contentRequest);
         content = service.getContentWithJPA(contentRequest);
+
+        kafkaTemplate.send("KafkaMessageLogger", content);
+        logger.info("Sent by Kafka: " + content);
 
         return content;
     }
